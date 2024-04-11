e52d5008ca9cb736463812c27bc2d93be6e239a4
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-11 10:01:49 +0200

    added github auth and implemented supabase for storing the blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export default function Home() {
+import AuthButton from "../components/AuthButton";
+
+export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
+  const { data: blogs } = await supabase.from("blogs").select("*");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
+      <AuthButton />
       <h1 className="text-5xl font-bold">Hello, World!</h1>
+      <pre>{JSON.stringify(blogs, null, 2)}</pre>
     </main>
   );
 }
---
+++ components/AuthButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export default function AuthButton() {
+  const supabase = createClientComponentClient();
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
+  };
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+  };
+  return (
+    <>
+      <button onClick={handleSignIn}>Sign In</button>
+      <button onClick={handleSignOut}>Sign Out</button>
+    </>
+  );
+}
